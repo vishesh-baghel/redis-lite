3f8b03cb5d44335a41dad67b943bdfe6f9c1d8fd
vishesh-baghel <visheshbaghel99@gmail.com>
2023-08-22 12:13:05 +0530

    ssh url added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("check if ssh is working for git or not");
     }
 }
